d460e940278712d4fe5973d8e86bd7678cf686cf
Justyna <justynakarolak0@gmail.com>
2026-03-09 11:59:18 +0100

    Delete sql directory

--- sql/dim_channel.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT
-  channelGrouping AS channel
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-WHERE channelGrouping IS NOT NULL
-ORDER BY channel ASC
--- sql/dim_device.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT
-    device.deviceCategory AS device
-  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-  WHERE device.deviceCategory IS NOT NULL
-  ORDER BY device ASC
